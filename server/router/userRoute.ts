426d48e2cf50ba6ff901395dcbdc79076e49c750
Cinque <cinque.ck@hotmail.com>
2018-07-19 23:57:15 +0800

    default to index

--- server/router/userRoute.ts
+++ server/router/userRoute.ts
@@ -10,6 +10,10 @@ export default function (router: Router, next) {
         await next.render(ctx.req, ctx.res, '/travels', ctx.query)
           ctx.respond = false
     })
+    router.get('/', async ctx => {
+        await next.render(ctx.req, ctx.res, '/index', ctx.query)
+          ctx.respond = false
+    })
 
     router.get('/user', userController.getUsers);
     router.post('/user/register', userController.register);
